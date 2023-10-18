f7162e7ba260b0f947a2aa9b87e9a1c17833b989
Miguel Ramos <miguel921433@gmail.com>
2023-10-18 12:06:19 -0500

    incio de proyecto lavarel

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' =>$this->faker->text(100),
+            'price'=>$this->faker->randowFloat(2,10,5000)
+        ];
+    }
+}
